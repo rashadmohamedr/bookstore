7a5efcd1cf614d522770a4f486715346b9ad1834
Rashad El Taher <rashadmohamed2019@icloud.com>
2026-02-05 03:11:07 +0200

    ch15 - Add search functionality with SearchResultsListView and update templates

--- books/views.py
+++ books/views.py
@@ -1,4 +1,7 @@
+from typing import Any
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin #look at the class implementation in the src
+from django.db.models import Q
+from django.db.models.query import QuerySet
 from django.views.generic import ListView, DetailView
 from .models import Book
 
@@ -13,4 +16,13 @@ class BookDetailView(LoginRequiredMixin,PermissionRequiredMixin, DetailView):
     template_name = "books/book_detail.html"
     login_url="account_login"
     permission_required="books.special_status"
+    
+class SearchResultsListView(ListView):
+    model = Book
+    template_name = "books/search_results.html"
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
     
